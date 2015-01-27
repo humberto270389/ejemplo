ff4063576a22b3a5b0f30753f295ddf0c9bdaa08
humberto270389 <humberto_270389@hotmail.com>
2015-01-27 12:01:21 -0700

    primer commit

---
+++ Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="jquery-1.11.2.min.js"></script>	
+</head>
+<body>
+<div id="AdivFuncion"></div>
+<form enctype="multipart/form-data" action="funcionSubir.php" method="POST">
+<input name="uploadedfile" type="file" />
+<input type="submit" value="Subir archivo" />
+</form>
+</body>
+</form>
+</body>
+</html>
